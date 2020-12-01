104f7e2b175a04084aa228b23b02741c27d27d74
Valentin Toader <valentin.toader@ibm.com>
2020-12-01 16:25:18 +0200

    service worker

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('online-store');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('online-store app is running!');
-  });
 });
